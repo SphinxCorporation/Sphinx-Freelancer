dda9f26af7a28e64cd058e262cb52fbb8eecf8c6
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-23 09:39:33 +0530

    [write] api and admin block

--- config/auth.php
+++ config/auth.php
@@ -40,7 +40,17 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins'
+        ],
     ],
+    
 
     /*
     |--------------------------------------------------------------------------
