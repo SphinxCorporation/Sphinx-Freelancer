9ed629c3482420cb3d07947bd08840542be801a2
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-23 09:36:53 +0530

    [config] base config -> my config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Sphix-Freelancer'),
 
     /*
     |--------------------------------------------------------------------------
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
